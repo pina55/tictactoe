5bc877bb1079170f9400315e71741b7895b5fa03
Pinar Tunc <pnar.tunc@hotmail.com>
2022-12-26 16:07:43 +0100

    initial commit

---
+++ src/components/NewGameButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./NewGameButton.css"
+
+export const NewGameButton = ({resetBoard}) => {
+  return (
+    <button className='newgame' onClick={resetBoard}>New Game</button>
+  )
+}
